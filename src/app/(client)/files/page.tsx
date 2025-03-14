abf8063b4d604db8a4ed5533d8060e15f5523c3e
Zav Zhang <hollowzheng@gmail.com>
2025-03-14 23:56:34 +0800

    new submit2

--- src/app/(client)/files/page.tsx
+++ src/app/(client)/files/page.tsx
@@ -1,25 +1,34 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./index.scss";
 import { useFiles, useSortable } from "./useHook";
 import { useRequest } from "ahooks";
-import { uploadFile, downloadMultipleFiles, changeFileName } from "./methods";
+import { downloadMultipleFiles, changeFileName } from "./methods";
 import { Input } from "./Input";
 
 const Index = () => {
   const dir = "";
-  const { data, loading } = useRequest(async () => {
-    //return await uploadFile({ dir });
-  });
+  const { data, loading } = useRequest(async () => {});
   const [isSubmit, setIsSubmit] = useState<boolean>(false);
 
   const { files, setFile, onFileChange } = useFiles(isSubmit, setIsSubmit);
 
   useSortable({ className: ".nav_class" });
 
+  function submitFile() {
+    setIsSubmit(true);
+    const DOM = document.querySelector(".nav_class") as Element;
+    const newFileList: any[] = [];
+    Array.from(DOM?.children).forEach((item, index) => {
+      const fileIndex = item?.getAttribute("index");
+      newFileList[index] = files[fileIndex];
+    });
+    downloadMultipleFiles(newFileList);
+  }
+
   return (
     <>
-      <header className="full h-40 flex">
+      <header className="full relative pt-5 box-border">
         <form>
           <input
             type="file"
@@ -29,27 +38,27 @@ const Index = () => {
             id="fileInput"
           />
         </form>
-        <label htmlFor="fileInput" className="inputFile">
-          选择文件
-        </label>
+        <div className="w-full">
+          <label
+            htmlFor="fileInput"
+            className="inputFile box-border rounded-custom-10 overflow-hidden block w-40 h-40 border-2 border-custom-gray relative left-1/2 translate-y-[-50%]"
+          >
+            <img
+              className="relative top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] "
+              src="./imgs/upload.png"
+            />
+          </label>
+        </div>
 
         <button
+          className="absolute right-0 top-1/2 translate-x-[-50%]"
           type="submit"
-          onClick={() => {
-            setIsSubmit(true);
-            const DOM = document.querySelector(".nav_class") as Element;
-            const newFileList: any[] = [];
-            Array.from(DOM?.children).forEach((item, index) => {
-              const fileIndex = item?.getAttribute("index");
-              newFileList[index] = files[fileIndex];
-            });
-            downloadMultipleFiles(newFileList);
-          }}
+          onClick={submitFile}
         >
           提交文件
         </button>
       </header>
-      <nav className="flex flex-wrap nav_class">
+      <nav className="flex flex-wrap nav_class border-t-2 border-black ">
         {Array.from(files, (file: any, index) => {
           const fileUrl = URL.createObjectURL(file);
           return (
